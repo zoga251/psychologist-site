f6236245b89a69ccaeaad6a0fbf1fa79b6e28028
zoga251 <medvedik235@gmail.com>
2026-02-21 22:43:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+document.querySelectorAll(".fade-up").forEach((el) => {
+  observer.observe(el);
+});
